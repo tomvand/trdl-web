1f64e84e49c05506fd0d6fc868723e05f4e2f76f
Tom van Dijk <tomvand@users.noreply.github.com>
2025-10-20 08:17:01 +0200

    Hello world! Rust project structure for cli and wasm

---
+++ rust/trdl/src/lib.rs
@@ -0,0 +1,17 @@
+pub struct RailNetwork {
+    // TODO: fields
+}
+
+pub struct Track;
+pub struct Points;
+pub struct Signal;
+
+impl RailNetwork {
+    pub fn new() -> Self {
+        Self { }
+    }
+}
+
+impl Track { pub fn new() -> Self { Track } }
+impl Points { pub fn new() -> Self { Points } }
+impl Signal { pub fn new() -> Self { Signal } }
