ed936715848e2a96ff45e3d965294a1af0df0fa2
tomvand <tomvand@users.noreply.github.com>
2026-03-11 06:59:51 +0100

    Restart as bare uv project

--- src/tests/doctest.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef DOCTEST_HPP
-#define DOCTEST_HPP
-
-#include "common.hpp"
-
-#include <doctest.h>
-
-#endif // DOCTEST_HPP
--- src/tests/main.cpp
+++
@@ -1,7 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.hpp"
-
-TEST_CASE("verify that doctest is working") {
-  REQUIRE(1 == 2 - 1);
-  REQUIRE_FALSE(1 == 0);
-}
